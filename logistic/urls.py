8b273ecd4b590acaeb2cfae9c0e05448f4d81160
Alenka <alena.vol100@gmail.com>
2024-03-16 22:43:08 +1000

    some files changed

--- logistic/urls.py
+++ logistic/urls.py
@@ -1,9 +1,11 @@
 from rest_framework.routers import DefaultRouter
-
-from logistic.views import ProductViewSet, StockViewSet
+from logistic.views import ProductViewSet, StockViewSet, sample_view
+from django.urls import path
 
 router = DefaultRouter()
 router.register('products', ProductViewSet)
 router.register('stocks', StockViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('test/', sample_view, name='sample'),
+] + router.urls
